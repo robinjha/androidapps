1921e861a93be2ca8b2d00268c56ddace9427ecf
robinjha <robinjha@gmail.com>
2015-06-21 14:07:55 -0400

    Complete calculator app

--- Calculator/src/org/example/calculator/MainActivity.java
+++ Calculator/src/org/example/calculator/MainActivity.java
@@ -4,13 +4,100 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	EditText operand1;
+	EditText operand2;
+	Button btnPlus;
+	Button btnSubtract;
+	Button btnMultiply;
+	Button btnDivide;
+	Button btnClr;
+	TextView result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		//operand fields on the main screen
+		operand1 = (EditText) findViewById(R.id.editOperand1);
+		operand2 = (EditText) findViewById(R.id.editOperand2);
+
+		//associate buttons
+		btnPlus = (Button) findViewById(R.id.btnPlus);
+		btnSubtract = (Button) findViewById(R.id.btnSubtract);
+		btnMultiply = (Button) findViewById(R.id.btnMultiply);
+		btnDivide = (Button) findViewById(R.id.btnDivide);
+		btnClr = (Button) findViewById(R.id.btnClr);
+
+		//associate result field
+		result = (TextView) findViewById(R.id.textResult);
+
+		//process result
+		btnPlus.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				float oper1 = Float.parseFloat(operand1.getText().toString());
+				float oper2 = Float.parseFloat(operand2.getText().toString());
+
+				float theResult = oper1 + oper2;
+				result.setText(Float.toString(theResult));
+			}
+		});
+
+		btnDivide.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				float oper1 = Float.parseFloat(operand1.getText().toString());
+				float oper2 = Float.parseFloat(operand2.getText().toString());
+
+				float theResult = oper1 / oper2;
+				result.setText(Float.toString(theResult));
+			}
+		});
+
+		btnSubtract.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				float oper1 = Float.parseFloat(operand1.getText().toString());
+				float oper2 = Float.parseFloat(operand2.getText().toString());
+
+				float theResult = oper1 - oper2;
+				result.setText(Float.toString(theResult));
+			}
+		});
+
+		btnMultiply.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				float oper1 = Float.parseFloat(operand1.getText().toString());
+				float oper2 = Float.parseFloat(operand2.getText().toString());
+
+				float theResult = oper1 * oper2;
+				result.setText(Float.toString(theResult));
+			}
+		});
+		
+		btnClr.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				operand1.setText("");
+				operand2.setText("");
+				result.setText("0.00");
+			}
+		});
+
 	}
 
 	@Override
