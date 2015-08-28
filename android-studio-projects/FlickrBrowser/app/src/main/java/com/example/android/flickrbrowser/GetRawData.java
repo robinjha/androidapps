53f09385635a63f6ca99cf422b3c0fe15835544d
robinjha <robinjha@gmail.com>
2015-08-27 23:55:03 -0400

    flickr app

---
+++ android-studio-projects/FlickrBrowser/app/src/main/java/com/example/android/flickrbrowser/GetRawData.java
@@ -0,0 +1,10 @@
+package com.example.android.flickrbrowser;
+
+/**
+ * Created by robin on 8/27/15.
+ */
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALISED, FAILED_OR_EMPTY, OK }
+
+public class GetRawData {
+}
